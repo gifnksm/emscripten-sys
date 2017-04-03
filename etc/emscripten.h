ac6a789a5f07d314ad174e72784bc5b300958eee
Yosuke ONOUE <onoue@likr-lab.com>
2017-04-03 11:23:08 +0900

    Add emscripten/*.h support

--- etc/emscripten.h
+++ etc/emscripten.h
@@ -1,1 +1,9 @@
 #include <emscripten.h>
+#include <emscripten/em_asm.h>
+#include <emscripten/fetch.h>
+#include <emscripten/html5.h>
+#include <emscripten/threading.h>
+#include <emscripten/trace.h>
+#include <emscripten/vector.h>
+#include <emscripten/vr.h>
+#include <emscripten/wire.h>
--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,9 @@
 
 extern crate core;
 
+extern "C" {
+    pub fn emscripten_GetProcAddress(name: *const std::os::raw::c_char)
+        -> *const std::os::raw::c_void;
+}
+
 include!(concat!(env!("OUT_DIR"), "/emscripten.rs"));
